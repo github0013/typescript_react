94b32579d0fd8b19c4fa5ca0011eab9481e136f0
ac <ac@domain.local>
2017-11-19 22:40:36 +0900

    add url-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.set('image', {
+  test: /\.(jpe?g|png|gif)$/i,
+  use: [{
+    loader: 'url-loader'
+  }]
+})
+
 module.exports = environment
